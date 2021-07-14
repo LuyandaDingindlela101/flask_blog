9d23a6ef38e9df1aa7294a3301c92fedd6c5c74f
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-07-14 21:50:45 +0200

    added layout file which holds the similar layout pieces accross pages

--- flask_blog.py
+++ flask_blog.py
@@ -3,17 +3,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Luyanda Dingindlela",
+        "title": "Blog title 1",
+        "content": "This is some content",
+        "date_posted": "April 20, 2021"
+    },
+    {
+        "author": "Bruce wayne",
+        "title": "Blog title 2",
+        "content": "This is some content",
+        "date_posted": "April 20, 2021"
+    },
+]
 
 # ADD A ROUTE DECORATOR TO TELL FLASK WHICH URL TO TRIGGER
 @app.route("/")
 @app.route("/home")
 def home():
-    return "This is home."
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "This is about."
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
